decf7dcd57cdaa8e004f9ab80383a6b37c316ef6
marquil-san <peacefulfemboy@gmail.com>
2026-07-18 09:50:57 +0530

    Works for the newer versions

--- spoofer.py
+++ spoofer.py
@@ -1,9 +1,9 @@
 # Feel free to modify (MIT License)
 
 import ctypes
-import uuid
 import ctypes.wintypes
-import os
+
+import clipboard
 import pymem
 import pymem.ressources.structure
 import time
@@ -16,6 +16,7 @@ valid_protections = (
     0x20,
     0x08
 )
+
 
 def pad_string(s, length):
     return s.encode('utf-8') + b'\x00' * (length - len(s))
@@ -60,25 +61,10 @@ def replace_unchanged_addresses(pm, addresses, target, b):
             pass
     return total_replacements
 
-def get_uuids_from_file():
-    path = os.path.expandvars(
-        r"C:\Users\%USERNAME%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftpe\hs"
-    )
-    if not os.path.exists(path):
-        return None, None
-
-    with open(path, 'r') as file:
-        lines = file.readlines()
-        first_line = lines[0].strip() if len(lines) > 0 else None
-        second_line = lines[1].strip() if len(lines) > 1 else None
-        return first_line, second_line
-
-
-
 def replace_memory(process_name, org):
     import pymem.process
 
-    spoofed_uuid = str(uuid.uuid4())
+    spoofed_uuid = "550e8400e29b41d4a716446655440000"    # replace with a custom uuid if u want to
     target = org.encode('utf-8')
     bytes = spoofed_uuid.encode('utf-8')
 
@@ -114,16 +100,8 @@ def replace_memory(process_name, org):
     return total, spoofed_uuid
 
 def spoof_minecraft_uuid():
-    first_uuid, second_uuid = get_uuids_from_file()
-    if not second_uuid:
+    uuid= str(clipboard.paste()).split('\n')[0][5:]
 
-        return
+    total, spoofed = replace_memory("Minecraft.Windows.exe", uuid)
 
-    total, spoofed = replace_memory("Minecraft.Windows.exe", second_uuid)
-
-    if total == 0 and first_uuid:
-        replace_memory("Minecraft.Windows.exe", first_uuid)
-
-
-# Start spoofing
 spoof_minecraft_uuid()
